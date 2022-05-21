cc07d7f47a1d401cb65bdf3718828ac5e6297078
OmarChaar <oishaar@gmail.com>
2022-05-21 20:30:04 -0300

    added custom styled button

--- components/ui/IconButton.js
+++ components/ui/IconButton.js
@@ -1,30 +1,24 @@
 import { StyleSheet, View, TouchableOpacity } from 'react-native';
-
 import { Ionicons } from '@expo/vector-icons'; 
 
-
-function addExpensesHandler() {
-    // navigation.navigate("Manage");
-    console.log("HERE")
-  }
-  
 function IconButton({ name, color, size, onPress }) {
-    return (
-        <TouchableOpacity onPress={onPress}>
-            <View style={styles.container}>
-                <Ionicons name={name} size={size} color={color} />
-            </View>
-        </TouchableOpacity>
-    )
+
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <View style={styles.container}>
+        <Ionicons name={name} size={size} color={color} />
+      </View>
+    </TouchableOpacity>
+  )
 }
 
 export default IconButton;
 
 const styles = StyleSheet.create({
-    container: {
-      borderRadius: 24,
-      padding: 6,
-      marginHorizontal: 8,
-      marginVertical: 2
-    },
-  });
+  container: {
+    borderRadius: 24,
+    padding: 6,
+    marginHorizontal: 8,
+    marginVertical: 2
+  },
+});
--- screens/stack/ManageExpenses.js
+++ screens/stack/ManageExpenses.js
@@ -1,6 +1,9 @@
 
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import { useLayoutEffect } from 'react'
+import IconButton from '../../components/ui/IconButton';
+import { GlobalStyles } from '../../contants/styles';
+import Button from '../../components/ui/Button';
 
 function ManageExpenses({navigation, route}) {
 
@@ -15,17 +18,34 @@ function ManageExpenses({navigation, route}) {
         })
     }, [navigation, isEditing]);
 
-    if(isEditing) {
-        return (
-            <View style={styles.container}>
-                <Text>EDITING EXPENSES</Text>
-            </View>
-        )
+    function deleteExpenseHandler() {
+        navigation.goBack();
+    }
+
+    function cancelHandler() {
+        navigation.goBack();
+    }
+
+    function confirmHandler() {
+        navigation.goBack();
     }
 
     return (
         <View style={styles.container}>
-            <Text>ADDING EXPENSES!</Text>
+            <View style={styles.buttonContainer}>
+                <Button style={styles.button} mode="flat" onPress={cancelHandler}>Cancel</Button>
+                <Button style={styles.button} onPress={confirmHandler}>{isEditing ? 'Update' : 'Add'}</Button>
+            </View>
+            {isEditing && (
+                <View style={styles.deleteContainer}>
+                    <IconButton 
+                        name="trash" 
+                        color={GlobalStyles.colors.error500} 
+                        size={36} 
+                        onPress={deleteExpenseHandler}
+                    />
+                </View>
+            )}
         </View>
     )
 }
@@ -34,6 +54,23 @@ export default ManageExpenses;
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1
+        flex: 1,
+        padding: 24,
+        backgroundColor: GlobalStyles.colors.primary800
     },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center'
+    },
+    button: {
+        minWidth: 120,
+        marginHorizontal: 8
+    },
+    deleteContainer: {
+        marginTop: 16,
+        paddingTop: 8,
+        borderTopWidth: 2,
+        borderTopColor: GlobalStyles.colors.primary200,
+        alignItems: 'center'
+    }
   });
